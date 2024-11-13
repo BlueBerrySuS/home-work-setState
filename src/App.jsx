14af79dbd0813494d34554e9f7c642c285d6b994
sussybaka <bluebarryus@gmail.com>
2024-11-13 08:54:53 +0600

    добавлен кликер

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import UserForm from './components/user-form/user-form'
+import Clicker from './components/clicker/clicker'
 
 function App() {
   return (
     <>
       <UserForm/>
+      <Clicker/>
     </>
   )
 }
---
+++ src/components/clicker/clicker.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import style from './clicker.module.css';
+
+function Clicker() {
+    const [clickCount, setClickCount] = useState(0);
+    const [fontColor, setFontColor] = useState("black");
+    const gradientColors = {
+        1: "#00FF00",
+        2: "#33FF00",
+        3: "#66FF00",
+        4: "#99FF00",
+        5: "#CCFF00",
+        6: "#FFCC00",
+        7: "#FF9900",
+        8: "#FF6600",
+        9: "#FF3300",
+        10: "#FF0000"
+    };
+
+    const handleClick = () => {
+        const newClickCount = clickCount === 10? 0 : clickCount + 1;
+        setClickCount(newClickCount);
+        setFontColor(gradientColors[newClickCount]);
+    };
+    return (
+        <>
+        <div className={style.clicker}>
+            <h2>Кликер</h2>
+            <h2 className={style.clicker_display} style={{color: fontColor}}>{clickCount}</h2>
+            <button className={style.clicker_button} onClick={handleClick}>Жми</button>
+        </div>
+        <hr />
+        </>
+    )
+}
+export default Clicker;
